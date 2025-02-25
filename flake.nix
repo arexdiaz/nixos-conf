064927dc52146227bae90df6cfc010329448b7b0
arexito <arexito@icloud.com>
2025-02-24 23:03:58 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    notion-app-electron = {
+      url = "github:arexdiaz/notion-app-electron";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p50
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+    };
+}
