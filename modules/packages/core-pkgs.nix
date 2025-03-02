3058786f1572808f3901525c775dfc893b75c8ed
arexdiaz <arexito@icloud.com>
2025-03-02 17:08:03 -0400

    Add nh package

--- modules/packages/core-pkgs.nix
+++ modules/packages/core-pkgs.nix
@@ -16,6 +16,11 @@
 
   # Nixos options
   programs = {
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+    };
+
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
     # mtr.enable = true;
