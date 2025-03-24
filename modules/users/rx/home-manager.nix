35113c7224f992c3382640e5882bc16b93657703
arexdiaz <arexito@icloud.com>
2025-03-24 00:04:34 -0400

    Add ovmf config

--- modules/users/rx/home-manager.nix
+++ modules/users/rx/home-manager.nix
@@ -55,6 +55,12 @@
       ".config/psd/psd.conf".text = ''
         USE_OVERLAYFS="yes"
       '';
+
+      ".config/libvirt/qemu.conf".text = if pkgs.qemu_kvm != null then ''
+        # Adapted from /var/lib/libvirt/qemu.conf
+        # Note that AAVMF and OVMF are for Aarch64 and x86 respectively
+        nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ];
+      '' else null;
     };
 
     # Home Manager can also manage your environment variables through
