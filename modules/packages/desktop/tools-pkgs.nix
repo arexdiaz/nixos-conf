822dd0099970e687acef096298e991552ecd31e1
arexdiaz <arexito@icloud.com>
2025-03-26 13:11:30 -0400

    Add ghidra

--- modules/packages/desktop/tools-pkgs.nix
+++ modules/packages/desktop/tools-pkgs.nix
@@ -1,12 +1,13 @@
 { config, pkgs, inputs, ... }:
 
 {
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-      burpsuite
-      nmap
-      remmina
-      wireshark
-    ];
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    burpsuite
+    nmap
+    remmina
+    wireshark
+    ghidra
+  ];
 }
