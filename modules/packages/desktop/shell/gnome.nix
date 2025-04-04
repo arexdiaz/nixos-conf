75e033795b10e41dbb8f9b7888a7c92468cbf6ef
arexdiaz <arexito@icloud.com>
2025-04-03 21:47:49 -0400

    Create gnome.nix. Includes experimental feature triple buffering

---
+++ modules/packages/desktop/shell/gnome.nix
@@ -0,0 +1,93 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  nixpkgs.overlays = [
+    # GNOME 47: triple-buffering-v4-47
+    (final: prev: {
+      mutter = prev.mutter.overrideAttrs (oldAttrs: {
+        # GNOME dynamic triple buffering (huge performance improvement)
+        # See https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441
+        # Also https://gitlab.gnome.org/vanvugt/mutter/-/tree/triple-buffering-v4-47
+        src = final.fetchFromGitLab {
+          domain = "gitlab.gnome.org";
+          owner = "vanvugt";
+          repo = "mutter";
+          rev = "triple-buffering-v4-47";
+          hash = "sha256-6n5HSbocU8QDwuhBvhRuvkUE4NflUiUKE0QQ5DJEzwI=";
+        };
+
+        # GNOME 47 requires the gvdb subproject which is not included in the triple-buffering branch
+        # This copies the necessary gvdb files from the official GNOME repository
+        preConfigure = let
+          gvdb = final.fetchFromGitLab {
+            domain = "gitlab.gnome.org";
+            owner = "GNOME";
+            repo = "gvdb";
+            rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
+            hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
+          };
+        in ''
+          cp -a "${gvdb}" ./subprojects/gvdb
+        '';
+      });
+    })
+  ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    orca
+    evince
+    # file-roller
+    geary
+    gnome-disk-utility
+    # seahorse
+    # sushi
+    # sysprof
+    #
+    # gnome-shell-extensions
+    #
+    # adwaita-icon-theme
+    # nixos-background-info
+    gnome-backgrounds
+    # gnome-bluetooth
+    # gnome-color-manager
+    # gnome-control-center
+    # gnome-shell-extensions
+    gnome-tour # GNOME Shell detects the .desktop file on first log-in.
+    gnome-user-docs
+    # glib # for gsettings program
+    # gnome-menus
+    # gtk3.out # for gtk-launch program
+    # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
+    # xdg-user-dirs-gtk # Used to create the default bookmarks
+    #
+    baobab
+    epiphany
+    gnome-text-editor
+    gnome-calculator
+    gnome-calendar
+    gnome-characters
+    # gnome-clocks
+    gnome-console
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    # gnome-system-monitor
+    gnome-weather
+    # loupe
+    # nautilus
+    gnome-connections
+    simple-scan
+    snapshot
+    totem
+    yelp
+    gnome-software
+  ];
+}
