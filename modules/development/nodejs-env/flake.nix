ec6d689229667074707c0ae27da5cc9b2f1a4f38
arexdiaz <arexito@icloud.com>
2025-04-06 15:21:53 -0400

    Add nodejs-env

---
+++ modules/development/nodejs-env/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Next.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          yarn
+          git
+        ];
+
+        shellHook = ''
+          echo "Welcome to your Next.js development shell!"
+        '';
+      };
+    };
+}
