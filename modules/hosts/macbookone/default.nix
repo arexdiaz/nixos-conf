1ff0e61a6b992c3b47fa22a5ff4ac78b52c86e39
arexdiaz <arexito@icloud.com>
2025-04-06 18:57:24 -0400

    Update macbookm1 config

--- modules/hosts/macbookone/default.nix
+++ modules/hosts/macbookone/default.nix
@@ -4,8 +4,8 @@
   imports = [
       ./hardware-configuration.nix
       ../../packages/core-pkgs.nix
+      ../../packages/desktop/shell/gnome.nix
       ../../packages/desktop/desktop-pkgs.nix
-      ../../packages/desktop/environments/gnome.nix
       ../../packages/fish-shell.nix
       ../../networking.nix
       ../../services.nix
@@ -13,7 +13,7 @@
       ../../users/rx
   ];
 
-  networking.hostName = "haiiii";
+  networking.hostName = "apple-m1";
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
