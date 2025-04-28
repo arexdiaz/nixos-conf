558c72c33eb4cf4982c422a2659455f4433d4aa0
arexdiaz <arexito@icloud.com>
2025-04-28 17:21:19 -0400

    Change default session for kde

--- modules/packages/desktop/shell/kde.nix
+++ modules/packages/desktop/shell/kde.nix
@@ -6,7 +6,7 @@
     desktopManager.plasma6.enable = true;
     displayManager = {
       sddm.enable = true;
-      defaultSession = "plasmax11";
+      defaultSession = "plasmawayland";
     };
   };
 
