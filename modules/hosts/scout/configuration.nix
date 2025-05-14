fbc2a0fb1b5f9a026420a147b1358c3308d34f37
arexdiaz <arexito@icloud.com>
2025-05-13 21:30:05 -0400

    Update scout host

---
+++ modules/hosts/scout/configuration.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "scout";
+  nixpkgs.config.allowUnfree = true;
+
+  boot.blacklistedKernelModules = [ "nouveau" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware = {
+    nvidia = {
+      open    = true;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+    };
+    graphics = {
+      enable        = true;
+      extraPackages = with pkgs; [nvidia-vaapi-driver];
+    };
+  };
+
+  # local packages
+  preconfs = {
+    desktop.environment.kde.enable  = true;
+    pkgs = {
+      kernel.cachyos.enable         = true;
+      core.enable                   = true;
+      common.enable                 = true;
+      games.enable                  = true;
+      media.enable                  = true;
+      tools.enable                  = true;
+      shell.fish.enable             = true;
+      emulators = {
+        enable                      = true;
+        wine.enable                 = true;
+        gaming = {
+          lutris.enable             = true;
+          pcsx2.enable              = true;
+        };
+      };
+    };
+  };
+
+  programs.nix-ld.enable = true;
+  powerManagement.cpuFreqGovernor = "performance";
+
+  services.plex = {
+    enable        = true;
+    openFirewall  = true;
+    user          = "rx";
+  };
+
+  fileSystems = {
+    "/mnt/Stuff" = {
+      device  = "/dev/disk/by-uuid/d4a092ab-ae4a-43b5-821b-3aa104a572a1";
+      fsType  = "ext4";
+      options = [ "users" "nofail" "exec" ];
+    };
+    "/mnt/Stuff2" = {
+      device  = "/dev/disk/by-uuid/CA33-738B";
+      fsType  = "vfat";
+      options = [ "users" "nofail" "exec" ];
+    };
+  };
+  
+  # services.thermald ={
+  #   enable = true;
+  # };
+
+  system.stateVersion = "24.11";
+}
--- modules/hosts/scout/default.nix
+++ modules/hosts/scout/default.nix
@@ -1,82 +1,11 @@
 { config, pkgs, lib, inputs, cRoot, ... }:
 
-let
-  modules = [
-    "common.nix"
-    "users/rx"
-  ];
-
-  # Prepend the common base path to each module file path
-  importModules = lib.map (path: "${cRoot}/modules/${path}") modules;
-in
 {
   imports = [
+    "${cRoot}/modules/configs"
+    "${cRoot}/modules/users/rx"
+    ./configuration.nix
     ./hardware-configuration.nix
     inputs.nix-flatpak.nixosModules.nix-flatpak # will be moved later on
-  ] ++ importModules;
-
-  networking.hostName = "scout";
-  nixpkgs.config.allowUnfree = true;
-
-  boot.blacklistedKernelModules = [ "nouveau" ];
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware = {
-    nvidia = {
-      open    = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
-    };
-    graphics = {
-      enable        = true;
-      extraPackages = with pkgs; [nvidia-vaapi-driver];
-    };
-  };
-
-  # local packages
-  preconfs = {
-    desktop.environment.kde.enable = true;
-    pkgs = {
-      core.enable       = true;
-      common.enable     = true;
-      games.enable      = true;
-      media.enable      = true;
-      tools.enable      = true;
-      shell.fish.enable = true;
-      emulators = {
-        enable          = true;
-        wine.enable     = true;
-        gaming = {
-          lutris.enable = true;
-          pcsx2.enable  = true;
-        };
-      };
-    };
-  };
-
-  programs.nix-ld.enable = true;
-  powerManagement.cpuFreqGovernor = "performance";
-
-  services.plex = {
-    enable        = true;
-    openFirewall  = true;
-    user          = "rx";
-  };
-
-  fileSystems = {
-    "/mnt/Stuff" = {
-      device  = "/dev/disk/by-uuid/d4a092ab-ae4a-43b5-821b-3aa104a572a1";
-      fsType  = "ext4";
-      options = [ "users" "nofail" "exec" ];
-    };
-    "/mnt/Stuff2" = {
-      device  = "/dev/disk/by-uuid/CA33-738B";
-      fsType  = "vfat";
-      options = [ "users" "nofail" "exec" ];
-    };
-  };
-  
-  # services.thermald ={
-  #   enable = true;
-  # };
-
-  system.stateVersion = "24.11";
+  ];
 }
