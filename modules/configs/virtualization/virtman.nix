b0a77b77f468bfe26ee4c3a32ed996e4d82d8475
arexdiaz <arexito@icloud.com>
2025-07-25 22:08:50 -0400

    Enable IP forwarding and configure trusted interfaces for virtualization

--- modules/configs/virtualization/virtman.nix
+++ modules/configs/virtualization/virtman.nix
@@ -22,6 +22,8 @@ in
 {
   config = lib.mkMerge [
     (lib.mkIf (virtualOptionConf.enable && virtualManagerCfg.enable) {
+      boot.kernel.sysctl."net.ipv4.ip_forward" = true;
+      networking.firewall.trustedInterfaces = [ "virbr0" ];
       programs.virt-manager.enable = true;
       users.groups.libvirtd.members = virtualManagerCfg.libvirtdMembers;
       environment.systemPackages = with pkgs;
